62dc7682bce07f12b0e3293338732386d83ea272
Galambos Mihály <galambos.misi@gmail.com>
2018-09-27 11:28:40 +0200

    Initial Commit

---
+++ Teszt/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Teszt
+//
+//  Created by Galambos Mihály on 2018. 09. 27..
+//  Copyright © 2018. Galambos Mihály. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
